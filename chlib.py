7732a15416cd747ec88e691c8dde4b777bc084e7
cellsheet <cellsheet@users.noreply.github.com>
2014-03-04 11:07:46 -0500

    Update chlib.py

--- chlib.py
+++ chlib.py
@@ -405,10 +405,6 @@ class ConnectionManager:
 			if int(bites[2]) > time.time():
 				group.sendCmd("msgbg", "1")
 
-		elif cmd == "getratelimit":
-			group.limit = int(bites[1])
-			group.limited = int(bites[2])
-
 		elif cmd == "g_participants":
 			pl = ":".join(bites[1:]).split(";")
 			for p in pl:
